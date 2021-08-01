45f20d125697943c887488f86a4586a9abf12f69
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2021-08-01 15:03:38 -0300

    Buscando objetos por identificador

--- src/test/java/com/wandson/ecommerce/iniciandocomjpa/ConsultandoRegistrosTest.java
+++ src/test/java/com/wandson/ecommerce/iniciandocomjpa/ConsultandoRegistrosTest.java
@@ -1,12 +1,15 @@
 package com.wandson.ecommerce.iniciandocomjpa;
 
+import com.wandson.ecommerce.model.Produto;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class ConsultandoRegistrosTest {
 
@@ -21,6 +24,24 @@ public class ConsultandoRegistrosTest {
     @BeforeEach
     public void setUp() {
         entityManager = entityManagerFactory.createEntityManager();
+    }
+
+    @Test
+    void buscarPorIdentificador() {
+        Produto produto = entityManager.find(Produto.class, 1);
+
+        Assertions.assertNotNull(produto);
+        Assertions.assertEquals("Kindle", produto.getNome());
+    }
+
+    @Test
+    void atualizarAReferencia() {
+        Produto produto = entityManager.find(Produto.class, 1);
+        produto.setNome("Microfone Samson");
+
+        entityManager.refresh(produto);
+
+        Assertions.assertEquals("Kindle", produto.getNome());
     }
 
     @AfterEach
