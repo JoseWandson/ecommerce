584cb0680926733792fde17272c0444563aefa91
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2021-08-01 15:52:26 -0300

    Criando uma classe genérica para testes

--- src/test/java/com/wandson/ecommerce/iniciandocomjpa/ConsultandoRegistrosTest.java
+++ src/test/java/com/wandson/ecommerce/iniciandocomjpa/ConsultandoRegistrosTest.java
@@ -1,30 +1,11 @@
 package com.wandson.ecommerce.iniciandocomjpa;
 
+import com.wandson.ecommerce.EntityManagerTest;
 import com.wandson.ecommerce.model.Produto;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class ConsultandoRegistrosTest {
-
-    private static EntityManagerFactory entityManagerFactory;
-    private EntityManager entityManager;
-
-    @BeforeAll
-    public static void setUpBeforeClass() {
-        entityManagerFactory = Persistence.createEntityManagerFactory("Ecommerce-PU");
-    }
-
-    @BeforeEach
-    public void setUp() {
-        entityManager = entityManagerFactory.createEntityManager();
-    }
+class ConsultandoRegistrosTest extends EntityManagerTest {
 
     @Test
     void buscarPorIdentificador() {
@@ -42,15 +23,5 @@ public class ConsultandoRegistrosTest {
         entityManager.refresh(produto);
 
         Assertions.assertEquals("Kindle", produto.getNome());
-    }
-
-    @AfterEach
-    public void tearDown() {
-        entityManager.close();
-    }
-
-    @AfterAll
-    public static void tearDownAfterClass() {
-        entityManagerFactory.close();
     }
 }
