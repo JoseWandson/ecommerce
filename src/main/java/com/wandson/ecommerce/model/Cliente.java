1faf050d7c4ef7db0a94e9fe925ceb1ce57b66f8
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2022-01-17 20:31:44 -0300

    Mapeando mapas com @ElementCollection

--- src/main/java/com/wandson/ecommerce/model/Cliente.java
+++ src/main/java/com/wandson/ecommerce/model/Cliente.java
@@ -1,11 +1,16 @@
 package com.wandson.ecommerce.model;
 
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapKeyColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.PostLoad;
 import jakarta.persistence.Table;
@@ -15,6 +20,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 @Entity
@@ -38,6 +44,12 @@ public class Cliente {
 
     @OneToMany(mappedBy = "cliente")
     private List<Pedido> pedidos;
+
+    @ElementCollection
+    @Column(name = "descricao")
+    @MapKeyColumn(name = "tipo")
+    @CollectionTable(name = "cliente_contato", joinColumns = @JoinColumn(name = "cliente_id"))
+    private Map<String, String> contatos;
 
     @PostLoad
     private void configurarPrimeiroNome() {
--- src/test/java/com/wandson/ecommerce/mapeamentoavancado/ElementCollectionTest.java
+++ src/test/java/com/wandson/ecommerce/mapeamentoavancado/ElementCollectionTest.java
@@ -2,11 +2,13 @@ package com.wandson.ecommerce.mapeamentoavancado;
 
 import com.wandson.ecommerce.EntityManagerTest;
 import com.wandson.ecommerce.model.Atributo;
+import com.wandson.ecommerce.model.Cliente;
 import com.wandson.ecommerce.model.Produto;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 class ElementCollectionTest extends EntityManagerTest {
@@ -39,5 +41,20 @@ class ElementCollectionTest extends EntityManagerTest {
 
         Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
         Assertions.assertFalse(produtoVerificacao.getAtributos().isEmpty());
+    }
+
+    @Test
+    void aplicarContato() {
+        entityManager.getTransaction().begin();
+
+        Cliente cliente = entityManager.find(Cliente.class, 1);
+        cliente.setContatos(Collections.singletonMap("email", "fernando@email.com"));
+
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Cliente clienteVerificacao = entityManager.find(Cliente.class, cliente.getId());
+        Assertions.assertEquals("fernando@email.com", clienteVerificacao.getContatos().get("email"));
     }
 }
