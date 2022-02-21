1128586762e7b14e6b958f1f99d6f3b399a86cae
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2022-02-21 20:41:55 -0300

    Configurando detalhes da tabela com @Table

---
+++ src/test/java/com/wandson/ecommerce/ddl/DDLTest.java
@@ -0,0 +1,12 @@
+package com.wandson.ecommerce.ddl;
+
+import com.wandson.ecommerce.EntityManagerTest;
+import org.junit.jupiter.api.Test;
+
+class DDLTest extends EntityManagerTest {
+
+    @Test
+    void gerarDDL() {
+        // Teste vazio... Somente para exibir DDL.
+    }
+}
