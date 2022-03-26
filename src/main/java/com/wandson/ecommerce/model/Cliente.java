65203dc9598895a161809d20c24ce4b002f8fd2e
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2022-03-26 20:48:25 -0300

    Configurando tabelas secundárias com @SecondaryTable

--- src/main/java/com/wandson/ecommerce/model/Cliente.java
+++ src/main/java/com/wandson/ecommerce/model/Cliente.java
@@ -30,7 +30,7 @@ import java.util.Objects;
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = true)
-@SecondaryTable(name = "cliente_detalhe", pkJoinColumns = @PrimaryKeyJoinColumn(name = "cliente_id"))
+@SecondaryTable(name = "cliente_detalhe", pkJoinColumns = @PrimaryKeyJoinColumn(name = "cliente_id"), foreignKey = @ForeignKey(name = "fk_cliente_detalhe_cliente"))
 @Table(name = "cliente", uniqueConstraints = @UniqueConstraint(name = "unq_cpf", columnNames = "cpf"), indexes = @Index(name = "idx_nome", columnList = "nome"))
 public class Cliente extends EntidadeBaseInteger {
 
