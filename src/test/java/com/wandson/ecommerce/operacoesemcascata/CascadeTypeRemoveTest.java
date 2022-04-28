5a7a05fdcec581b6b7eeed05fcc23cce004d2e96
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2022-04-28 11:07:58 -0300

    Removendo a relação de Produto e Categoria com @ManyToMany.

--- src/test/java/com/wandson/ecommerce/operacoesemcascata/CascadeTypeRemoveTest.java
+++ src/test/java/com/wandson/ecommerce/operacoesemcascata/CascadeTypeRemoveTest.java
@@ -4,6 +4,7 @@ import com.wandson.ecommerce.EntityManagerTest;
 import com.wandson.ecommerce.model.ItemPedido;
 import com.wandson.ecommerce.model.ItemPedidoId;
 import com.wandson.ecommerce.model.Pedido;
+import com.wandson.ecommerce.model.Produto;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -39,5 +40,21 @@ class CascadeTypeRemoveTest extends EntityManagerTest {
 
         Pedido pedidoVerificacao = entityManager.find(Pedido.class, itemPedido.getPedido().getId());
         Assertions.assertNull(pedidoVerificacao);
+    }
+
+    @Test
+    void removerRelacaoProdutoCategoria() {
+        Produto produto = entityManager.find(Produto.class, 1);
+
+        Assertions.assertFalse(produto.getCategorias().isEmpty());
+
+        entityManager.getTransaction().begin();
+        produto.getCategorias().clear();
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+        Assertions.assertTrue(produtoVerificacao.getCategorias().isEmpty());
     }
 }
