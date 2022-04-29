d645cfefdf4c7c2962dd314003e65ef79dd8fe82
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2022-04-29 12:06:27 -0300

    Selecionando um atributo da entidade como retorno da consulta

--- src/test/java/com/wandson/ecommerce/jpql/BasicoJPQLTest.java
+++ src/test/java/com/wandson/ecommerce/jpql/BasicoJPQLTest.java
@@ -1,11 +1,14 @@
 package com.wandson.ecommerce.jpql;
 
 import com.wandson.ecommerce.EntityManagerTest;
+import com.wandson.ecommerce.model.Cliente;
 import com.wandson.ecommerce.model.Pedido;
 import jakarta.persistence.Query;
 import jakarta.persistence.TypedQuery;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 class BasicoJPQLTest extends EntityManagerTest {
 
@@ -31,5 +34,19 @@ class BasicoJPQLTest extends EntityManagerTest {
         Query query = entityManager.createQuery(jpql);
         Pedido pedido2 = (Pedido) query.getSingleResult();
         Assertions.assertNotNull(pedido2);
+    }
+
+    @Test
+    void selecionarUmAtributoParaRetorno() {
+        String jpql = "select p.nome from Produto p";
+
+        TypedQuery<String> typedQuery = entityManager.createQuery(jpql, String.class);
+        List<String> lista = typedQuery.getResultList();
+        Assertions.assertEquals(String.class, lista.get(0).getClass());
+
+        String jpqlCliente = "select p.cliente from Pedido p";
+        TypedQuery<Cliente> typedQueryCliente = entityManager.createQuery(jpqlCliente, Cliente.class);
+        List<Cliente> listaClientes = typedQueryCliente.getResultList();
+        Assertions.assertEquals(Cliente.class, listaClientes.get(0).getClass());
     }
 }
