fc46457902e9c2eb224cc0c6e2418540649268b2
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2022-05-12 14:42:47 -0300

    Fazendo paginação de resultados com jpql

---
+++ src/test/java/com/wandson/ecommerce/jpql/PaginacaoJPQLTest.java
@@ -0,0 +1,28 @@
+package com.wandson.ecommerce.jpql;
+
+import com.wandson.ecommerce.EntityManagerTest;
+import com.wandson.ecommerce.model.Categoria;
+import jakarta.persistence.TypedQuery;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class PaginacaoJPQLTest extends EntityManagerTest {
+
+    @Test
+    void paginarResultados() {
+        String jpql = "select c from Categoria c order by c.nome";
+
+        TypedQuery<Categoria> typedQuery = entityManager.createQuery(jpql, Categoria.class);
+
+        // FIRST_RESULT = MAX_RESULTS * (pagina - 1)
+        typedQuery.setFirstResult(6);
+        typedQuery.setMaxResults(2);
+
+        List<Categoria> lista = typedQuery.getResultList();
+        Assertions.assertFalse(lista.isEmpty());
+
+        lista.forEach(c -> System.out.println(c.getId() + ", " + c.getNome()));
+    }
+}
