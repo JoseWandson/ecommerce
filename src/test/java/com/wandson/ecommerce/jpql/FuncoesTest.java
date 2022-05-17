6fe5b99b91f2790ec9a407bdb457d8edf9563c39
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2022-05-17 17:22:59 -0300

    Usando funções de agregação com jpql; Agrupando o registros com group by em jpql.

--- src/test/java/com/wandson/ecommerce/jpql/FuncoesTest.java
+++ src/test/java/com/wandson/ecommerce/jpql/FuncoesTest.java
@@ -13,7 +13,7 @@ class FuncoesTest extends EntityManagerTest {
     void aplicarFuncaoString() {
         // concat, length, locate, substring, lower, upper, trim
 
-        String jpql = "select c.nome, length(c.nome) from Categoria c where substring(c.nome, 1, 1) = 'N'";
+        var jpql = "select c.nome, length(c.nome) from Categoria c where substring(c.nome, 1, 1) = 'N'";
 
         TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
 
@@ -29,7 +29,7 @@ class FuncoesTest extends EntityManagerTest {
         // current_date, current_time, current_timestamp
         // year(p.dataCriacao), month(p.dataCriacao), day(p.dataCriacao)
 
-        String jpql = "select hour(p.dataCriacao), minute(p.dataCriacao), second(p.dataCriacao) from Pedido p where hour(p.dataCriacao) > 12";
+        var jpql = "select hour(p.dataCriacao), minute(p.dataCriacao), second(p.dataCriacao) from Pedido p where hour(p.dataCriacao) > 12";
 
         TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
 
@@ -41,7 +41,7 @@ class FuncoesTest extends EntityManagerTest {
 
     @Test
     void aplicarFuncaoNumero() {
-        String jpql = "select abs(p.total), mod(p.id, 2), sqrt(p.total) from Pedido p where abs(p.total) > 1000";
+        var jpql = "select abs(p.total), mod(p.id, 2), sqrt(p.total) from Pedido p where abs(p.total) > 1000";
 
         TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
 
@@ -53,7 +53,7 @@ class FuncoesTest extends EntityManagerTest {
 
     @Test
     void aplicarFuncaoColecao() {
-        String jpql = "select size(p.itens) from Pedido p where size(p.itens) > 1";
+        var jpql = "select size(p.itens) from Pedido p where size(p.itens) > 1";
 
         TypedQuery<Integer> typedQuery = entityManager.createQuery(jpql, Integer.class);
 
@@ -65,11 +65,25 @@ class FuncoesTest extends EntityManagerTest {
 
     @Test
     void aplicarFuncaoNativas() {
-        String jpql = "select function('dayname', p.dataCriacao) from Pedido p where function('acima_media_faturamento', p.total) = 1";
+        var jpql = "select function('dayname', p.dataCriacao) from Pedido p where function('acima_media_faturamento', p.total) = 1";
 
         TypedQuery<String> typedQuery = entityManager.createQuery(jpql, String.class);
 
         List<String> lista = typedQuery.getResultList();
+        Assertions.assertFalse(lista.isEmpty());
+
+        lista.forEach(System.out::println);
+    }
+
+    @Test
+    void aplicarFuncaoAgregacao() {
+        // avg, count, min, max, sum
+
+        var jpql = "select sum(p.total) from Pedido p";
+
+        TypedQuery<Number> typedQuery = entityManager.createQuery(jpql, Number.class);
+
+        List<Number> lista = typedQuery.getResultList();
         Assertions.assertFalse(lista.isEmpty());
 
         lista.forEach(System.out::println);
---
+++ src/test/java/com/wandson/ecommerce/jpql/GroupByTest.java
@@ -0,0 +1,97 @@
+package com.wandson.ecommerce.jpql;
+
+import com.wandson.ecommerce.EntityManagerTest;
+import jakarta.persistence.TypedQuery;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class GroupByTest extends EntityManagerTest {
+
+    @Test
+    void agruparResultado_quantidadeDeProdutospPorCategoria() {
+        var jpql = "select c.nome, count(p.id) from Categoria c join c.produtos p group by c.id";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+
+        Assertions.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
+    }
+
+    @Test
+    void agruparResultado_totalDeVendasPorMes() {
+        var jpql = """
+                select concat(year(p.dataCriacao), '/', function('monthname', p.dataCriacao)), sum(p.total)
+                from Pedido p
+                group by year (p.dataCriacao), month (p.dataCriacao)""";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+
+        Assertions.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
+    }
+
+    @Test
+    void agruparResultado_totalDeVendasPorCategoria() {
+        var jpql = """
+                select c.nome, sum(ip.precoProduto)
+                from ItemPedido ip
+                         join ip.produto pro
+                         join pro.categorias c
+                group by c.id""";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+
+        Assertions.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
+    }
+
+    @Test
+    void agruparResultado_totalDeVendasPorCliente() {
+        var jpql = """
+                select c.nome, sum(ip.precoProduto)
+                from ItemPedido ip
+                         join ip.pedido p
+                         join p.cliente c
+                group by c.id""";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+
+        Assertions.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
+    }
+
+    @Test
+    void agruparResultado_totalDeVendasPorDiaEPorCategoria() {
+        var jpql = """
+                select concat(year(p.dataCriacao), '/', month(p.dataCriacao), '/', day(p.dataCriacao)),
+                       concat(c.nome, ': ', sum(ip.precoProduto))
+                from ItemPedido ip
+                         join ip.pedido p
+                         join ip.produto pro
+                         join pro.categorias c
+                group by year (p.dataCriacao), month (p.dataCriacao), day (p.dataCriacao), c.id
+                order by p.dataCriacao, c.nome""";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+
+        Assertions.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
+    }
+}
