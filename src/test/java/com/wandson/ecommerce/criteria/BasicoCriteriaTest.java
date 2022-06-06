8c8c88508b004a515d7ffc7d9278b97c09762a30
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2022-06-06 18:14:32 -0300

    Buscando pelo identificador usando Criteria API do JPA

---
+++ src/test/java/com/wandson/ecommerce/criteria/BasicoCriteriaTest.java
@@ -0,0 +1,28 @@
+package com.wandson.ecommerce.criteria;
+
+import com.wandson.ecommerce.EntityManagerTest;
+import com.wandson.ecommerce.model.Pedido;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class BasicoCriteriaTest extends EntityManagerTest {
+
+    @Test
+    void buscarPorIdentificador() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        criteriaQuery.select(root);
+        criteriaQuery.where(criteriaBuilder.equal(root.get("id"), 1));
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        Pedido pedido = typedQuery.getSingleResult();
+        Assertions.assertNotNull(pedido);
+    }
+}
