dd4b6d16aa035e8c23002526f5affffb4a11a201
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2022-06-12 16:22:09 -0300

    Usando a cláusula on no join em criteria

--- src/test/java/com/wandson/ecommerce/criteria/JoinCriteriaTest.java
+++ src/test/java/com/wandson/ecommerce/criteria/JoinCriteriaTest.java
@@ -1,10 +1,13 @@
 package com.wandson.ecommerce.criteria;
 
 import com.wandson.ecommerce.EntityManagerTest;
+import com.wandson.ecommerce.model.Pagamento;
 import com.wandson.ecommerce.model.Pedido;
+import com.wandson.ecommerce.model.StatusPagamento;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
 import jakarta.persistence.criteria.Root;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -26,5 +29,21 @@ class JoinCriteriaTest extends EntityManagerTest {
 
         List<Pedido> lista = typedQuery.getResultList();
         Assertions.assertEquals(4, lista.size());
+    }
+
+    @Test
+    void fazerJoinComOn() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+        Join<Pedido, Pagamento> joinPagamento = root.join("pagamento");
+        joinPagamento.on(criteriaBuilder.equal(joinPagamento.get("status"), StatusPagamento.PROCESSANDO));
+
+        criteriaQuery.select(root);
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        List<Pedido> lista = typedQuery.getResultList();
+        Assertions.assertEquals(2, lista.size());
     }
 }
