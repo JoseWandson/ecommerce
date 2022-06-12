1fe455c03e1efe5adf2395ddc1c239eefd0c495d
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2022-06-12 17:31:06 -0300

    Usando left outer join em criteria

--- src/test/java/com/wandson/ecommerce/criteria/JoinCriteriaTest.java
+++ src/test/java/com/wandson/ecommerce/criteria/JoinCriteriaTest.java
@@ -8,6 +8,7 @@ import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
 import jakarta.persistence.criteria.Root;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -45,5 +46,20 @@ class JoinCriteriaTest extends EntityManagerTest {
 
         List<Pedido> lista = typedQuery.getResultList();
         Assertions.assertEquals(2, lista.size());
+    }
+
+    @Test
+    void fazerLeftOuterJoin() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+        root.join("pagamento", JoinType.LEFT);
+
+        criteriaQuery.select(root);
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        List<Pedido> lista = typedQuery.getResultList();
+        Assertions.assertEquals(5, lista.size());
     }
 }
