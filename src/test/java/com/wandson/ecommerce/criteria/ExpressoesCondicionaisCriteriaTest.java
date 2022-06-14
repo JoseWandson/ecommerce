d92f47049ea97a90e73d85687bb54ee8b6d3ea55
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2022-06-14 14:20:08 -0300

    Usando as expressões condicionais is null e is empty em criteria

--- src/test/java/com/wandson/ecommerce/criteria/ExpressoesCondicionaisCriteriaTest.java
+++ src/test/java/com/wandson/ecommerce/criteria/ExpressoesCondicionaisCriteriaTest.java
@@ -3,6 +3,8 @@ package com.wandson.ecommerce.criteria;
 import com.wandson.ecommerce.EntityManagerTest;
 import com.wandson.ecommerce.model.Cliente;
 import com.wandson.ecommerce.model.Cliente_;
+import com.wandson.ecommerce.model.Produto;
+import com.wandson.ecommerce.model.Produto_;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
@@ -27,4 +29,33 @@ class ExpressoesCondicionaisCriteriaTest extends EntityManagerTest {
         List<Cliente> lista = typedQuery.getResultList();
         Assertions.assertFalse(lista.isEmpty());
     }
+
+    @Test
+    void usarIsEmpty() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Produto> criteriaQuery = criteriaBuilder.createQuery(Produto.class);
+        Root<Produto> root = criteriaQuery.from(Produto.class);
+
+        criteriaQuery.select(root);
+        criteriaQuery.where(criteriaBuilder.isNotEmpty(root.get(Produto_.categorias)));
+
+        TypedQuery<Produto> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Produto> lista = typedQuery.getResultList();
+        Assertions.assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    void usarIsNull() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Produto> criteriaQuery = criteriaBuilder.createQuery(Produto.class);
+        Root<Produto> root = criteriaQuery.from(Produto.class);
+
+        criteriaQuery.select(root);
+        criteriaQuery.where(criteriaBuilder.isNull(root.get(Produto_.foto)));
+
+        TypedQuery<Produto> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Produto> lista = typedQuery.getResultList();
+        Assertions.assertFalse(lista.isEmpty());
+    }
+
 }
