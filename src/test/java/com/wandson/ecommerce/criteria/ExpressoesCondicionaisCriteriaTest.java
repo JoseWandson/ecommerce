01402bb2d611d464ef34f2c91d47c75723269184
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2022-06-14 11:51:13 -0300

    Usando expressão condicional like em criteria

---
+++ src/test/java/com/wandson/ecommerce/criteria/ExpressoesCondicionaisCriteriaTest.java
@@ -0,0 +1,30 @@
+package com.wandson.ecommerce.criteria;
+
+import com.wandson.ecommerce.EntityManagerTest;
+import com.wandson.ecommerce.model.Cliente;
+import com.wandson.ecommerce.model.Cliente_;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class ExpressoesCondicionaisCriteriaTest extends EntityManagerTest {
+
+    @Test
+    void usarExpressaoCondicionalLike() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Cliente> criteriaQuery = criteriaBuilder.createQuery(Cliente.class);
+        Root<Cliente> root = criteriaQuery.from(Cliente.class);
+
+        criteriaQuery.select(root);
+        criteriaQuery.where(criteriaBuilder.like(root.get(Cliente_.nome), "%a%"));
+
+        TypedQuery<Cliente> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Cliente> lista = typedQuery.getResultList();
+        Assertions.assertFalse(lista.isEmpty());
+    }
+}
