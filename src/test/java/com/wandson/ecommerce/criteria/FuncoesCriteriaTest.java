940e0c551b2599e1a43469ea9dea00e5c7840b7e
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2022-06-24 10:39:02 -0300

    Usando funções nativas em criteria

--- src/test/java/com/wandson/ecommerce/criteria/FuncoesCriteriaTest.java
+++ src/test/java/com/wandson/ecommerce/criteria/FuncoesCriteriaTest.java
@@ -126,4 +126,26 @@ class FuncoesCriteriaTest extends EntityManagerTest {
 
         lista.forEach(arr -> System.out.println(arr[0] + ", size: " + arr[1]));
     }
+
+    @Test
+    void aplicarFuncaoNativas() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        criteriaQuery.multiselect(
+                root.get(Pedido_.id),
+                criteriaBuilder.function("dayname", String.class, root.get(Pedido_.dataCriacao))
+        );
+        criteriaQuery.where(criteriaBuilder.isTrue(
+                criteriaBuilder.function(
+                        "acima_media_faturamento", Boolean.class, root.get(Pedido_.total))));
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Object[]> lista = typedQuery.getResultList();
+        Assertions.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + ", dayname: " + arr[1]));
+    }
+
 }
