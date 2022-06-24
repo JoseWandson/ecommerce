1ce9ff99593ad91210a41b6b2b7562768943d2fa
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2022-06-24 16:48:23 -0300

    Usando funções de agregação em criteria

--- src/test/java/com/wandson/ecommerce/criteria/FuncoesCriteriaTest.java
+++ src/test/java/com/wandson/ecommerce/criteria/FuncoesCriteriaTest.java
@@ -148,4 +148,31 @@ class FuncoesCriteriaTest extends EntityManagerTest {
         lista.forEach(arr -> System.out.println(arr[0] + ", dayname: " + arr[1]));
     }
 
+    @Test
+    void aplicarFuncaoAgregacao() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        criteriaQuery.multiselect(
+                criteriaBuilder.count(root.get(Pedido_.id)),
+                criteriaBuilder.avg(root.get(Pedido_.total)),
+                criteriaBuilder.sum(root.get(Pedido_.total)),
+                criteriaBuilder.min(root.get(Pedido_.total)),
+                criteriaBuilder.max(root.get(Pedido_.total))
+        );
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Object[]> lista = typedQuery.getResultList();
+        Assertions.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(
+                "count: " + arr[0]
+                        + ", avg: " + arr[1]
+                        + ", sum: " + arr[2]
+                        + ", min: " + arr[3]
+                        + ", max: " + arr[4]));
+    }
+
+
 }
