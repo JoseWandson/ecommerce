69f928521d30354baeef39db1f758d8e9a5505e1
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2022-06-27 14:45:25 -0300

    Consultando pedidos com produto específico em criteria

--- src/test/java/com/wandson/ecommerce/criteria/PathExpressionsTest.java
+++ src/test/java/com/wandson/ecommerce/criteria/PathExpressionsTest.java
@@ -2,11 +2,15 @@ package com.wandson.ecommerce.criteria;
 
 import com.wandson.ecommerce.EntityManagerTest;
 import com.wandson.ecommerce.model.Cliente_;
+import com.wandson.ecommerce.model.ItemPedido;
+import com.wandson.ecommerce.model.ItemPedido_;
 import com.wandson.ecommerce.model.Pedido;
 import com.wandson.ecommerce.model.Pedido_;
+import com.wandson.ecommerce.model.Produto_;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
 import jakarta.persistence.criteria.Root;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -23,6 +27,21 @@ class PathExpressionsTest extends EntityManagerTest {
 
         criteriaQuery.select(root);
         criteriaQuery.where(criteriaBuilder.like(root.get(Pedido_.cliente).get(Cliente_.nome), "M%"));
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Pedido> lista = typedQuery.getResultList();
+        Assertions.assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    void buscarPedidosComProdutoDeIDIgual1Exercicio() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+        Join<Pedido, ItemPedido> join = root.join(Pedido_.itens);
+
+        criteriaQuery.select(root);
+        criteriaQuery.where(criteriaBuilder.equal(join.get(ItemPedido_.produto).get(Produto_.id), 1));
 
         TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
         List<Pedido> lista = typedQuery.getResultList();
