74d98c3ca44a679023068f9fa0d130c3d60a57a6
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2022-06-27 21:46:49 -0300

    Agrupando registros com funções no group by em criteria

--- src/test/java/com/wandson/ecommerce/criteria/GroupByCriteriaTest.java
+++ src/test/java/com/wandson/ecommerce/criteria/GroupByCriteriaTest.java
@@ -14,6 +14,7 @@ import com.wandson.ecommerce.model.Produto_;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Expression;
 import jakarta.persistence.criteria.Join;
 import jakarta.persistence.criteria.JoinType;
 import jakarta.persistence.criteria.Root;
@@ -27,7 +28,7 @@ class GroupByCriteriaTest extends EntityManagerTest {
     @Test
     void agruparResultado02() {
 //        Total de vendas por categoria.
-//        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip join ip.produto pro join pro.categorias c group by c.id";
+//        select c.nome, sum(ip.precoProduto) from ItemPedido ip join ip.produto pro join pro.categorias c group by c.id
 
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
@@ -52,7 +53,7 @@ class GroupByCriteriaTest extends EntityManagerTest {
     @Test
     void agruparResultado01() {
 //        Quantidade de produtos por categoria.
-//        String jpql = "select c.nome, count(p.id) from Categoria c join c.produtos p group by c.id";
+//        select c.nome, count(p.id) from Categoria c join c.produtos p group by c.id
 
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
@@ -75,7 +76,7 @@ class GroupByCriteriaTest extends EntityManagerTest {
     @Test
     void agruparResultado03() {
 //        Total de vendas por cliente
-//        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip join ip.pedido p join p.cliente c group by c.id";
+//        select c.nome, sum(ip.precoProduto) from ItemPedido ip join ip.pedido p join p.cliente c group by c.id
 
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
@@ -94,6 +95,36 @@ class GroupByCriteriaTest extends EntityManagerTest {
         Assertions.assertFalse(lista.isEmpty());
 
         lista.forEach(arr -> System.out.println("Nome cliente: " + arr[0] + ", Sum: " + arr[1]));
+    }
 
+    @Test
+    void agruparResultadoComFuncoes() {
+//        Total de vendas por mês.
+//        select concat(year(p.dataCriacao), '/', function('monthname', p.dataCriacao)), sum(p.total) from Pedido p group by year(p.dataCriacao), month(p.dataCriacao)
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        Expression<Integer> anoCriacaoPedido = criteriaBuilder
+                .function("year", Integer.class, root.get(Pedido_.dataCriacao));
+        Expression<Integer> mesCriacaoPedido = criteriaBuilder
+                .function("month", Integer.class, root.get(Pedido_.dataCriacao));
+        Expression<String> nomeMesCriacaoPedido = criteriaBuilder
+                .function("monthname", String.class, root.get(Pedido_.dataCriacao));
+
+        Expression<String> anoMesConcat = criteriaBuilder.concat(
+                criteriaBuilder.concat(anoCriacaoPedido.as(String.class), "/"),
+                nomeMesCriacaoPedido
+        );
+
+        criteriaQuery.multiselect(anoMesConcat, criteriaBuilder.sum(root.get(Pedido_.total)));
+        criteriaQuery.groupBy(anoCriacaoPedido, mesCriacaoPedido);
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Object[]> lista = typedQuery.getResultList();
+        Assertions.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println("Ano/Mês: " + arr[0] + ", Sum: " + arr[1]));
     }
 }
