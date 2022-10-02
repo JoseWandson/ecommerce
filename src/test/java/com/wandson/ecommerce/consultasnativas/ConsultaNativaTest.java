cec6fe5b6f5fecfa90bdf1aa563a289a68767583
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2022-10-02 20:39:56 -0300

    Executando SQL e retornando uma entidade

--- src/test/java/com/wandson/ecommerce/consultasnativas/ConsultaNativaTest.java
+++ src/test/java/com/wandson/ecommerce/consultasnativas/ConsultaNativaTest.java
@@ -1,11 +1,18 @@
 package com.wandson.ecommerce.consultasnativas;
 
 import com.wandson.ecommerce.EntityManagerTest;
+import com.wandson.ecommerce.model.Produto;
 import jakarta.persistence.Query;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class ConsultaNativaTest extends EntityManagerTest {
 
@@ -19,5 +26,73 @@ class ConsultaNativaTest extends EntityManagerTest {
         Assertions.assertFalse(lista.isEmpty());
 
         lista.forEach(arr -> System.out.printf("Produto => ID: %s, Nome: %s%n", arr[0], arr[1]));
+    }
+
+    @ParameterizedTest
+    @MethodSource("getSqls")
+    @SuppressWarnings("unchecked")
+    void executarSQLRetornandoEntidade(String sql) {
+        Query query = entityManager.createNativeQuery(sql, Produto.class);
+
+        List<Produto> lista = query.getResultList();
+        Assertions.assertFalse(lista.isEmpty());
+
+        lista.forEach(obj -> System.out.printf("Produto => ID: %s, Nome: %s%n", obj.getId(), obj.getNome()));
+    }
+
+    private static Stream<Arguments> getSqls() {
+//        Produtos da tabela produto
+        var sql1 = """
+                SELECT
+                    id,
+                    nome,
+                    descricao,
+                    data_criacao,
+                    data_ultima_atualizacao,
+                    preco,
+                    foto
+                FROM
+                    produto""";
+
+//         Produtos da tabela produto_loja
+        var sql2 = """
+                SELECT
+                    id,
+                    nome,
+                    descricao,
+                    data_criacao,
+                    data_ultima_atualizacao,
+                    preco,
+                    foto
+                FROM
+                    produto_loja""";
+
+//         Produtos da tabela ecm_produto
+        var sql3 = """
+                SELECT
+                    prd_id id,
+                    prd_nome nome,
+                    prd_descricao descricao,
+                    prd_data_criacao data_criacao,
+                    prd_data_ultima_atualizacao data_ultima_atualizacao,
+                    prd_preco preco,
+                    prd_foto foto
+                FROM
+                    ecm_produto""";
+
+//         Produtos da tabela erp_produto
+        var sql4 = """
+                SELECT
+                    id,
+                    nome,
+                    descricao,
+                    NULL data_criacao,
+                    NULL data_ultima_atualizacao,
+                    preco,
+                    NULL foto
+                FROM
+                    erp_produto""";
+
+        return Stream.of(arguments(sql1), arguments(sql2), arguments(sql3), arguments(sql4));
     }
 }
