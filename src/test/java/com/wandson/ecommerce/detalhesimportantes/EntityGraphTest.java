71cd6d15b70b1f8038bf69e86807722cb699670b
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2023-02-24 09:24:31 -0300

    Adicionando um Subgraph na consulta

--- src/test/java/com/wandson/ecommerce/detalhesimportantes/EntityGraphTest.java
+++ src/test/java/com/wandson/ecommerce/detalhesimportantes/EntityGraphTest.java
@@ -1,8 +1,10 @@
 package com.wandson.ecommerce.detalhesimportantes;
 
 import com.wandson.ecommerce.EntityManagerTest;
+import com.wandson.ecommerce.model.Cliente;
 import com.wandson.ecommerce.model.Pedido;
 import jakarta.persistence.EntityGraph;
+import jakarta.persistence.Subgraph;
 import jakarta.persistence.TypedQuery;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -29,6 +31,20 @@ class EntityGraphTest extends EntityManagerTest {
     void buscarAtributosEssenciaisDePedidoComJPQL() {
         EntityGraph<Pedido> entityGraph = entityManager.createEntityGraph(Pedido.class);
         entityGraph.addAttributeNodes("notaFiscal", "cliente");
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery("select p from Pedido p", Pedido.class);
+        typedQuery.setHint("jakarta.persistence.fetchgraph", entityGraph);
+        List<Pedido> lista = typedQuery.getResultList();
+        Assertions.assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    void buscarAtributosEssenciaisDePedidoComJPQL02() {
+        EntityGraph<Pedido> entityGraph = entityManager.createEntityGraph(Pedido.class);
+        entityGraph.addAttributeNodes("dataCriacao", "status", "total");
+
+        Subgraph<Cliente> subgraphCliente = entityGraph.addSubgraph("cliente", Cliente.class);
+        subgraphCliente.addAttributeNodes("nome", "cpf");
 
         TypedQuery<Pedido> typedQuery = entityManager.createQuery("select p from Pedido p", Pedido.class);
         typedQuery.setHint("jakarta.persistence.fetchgraph", entityGraph);
