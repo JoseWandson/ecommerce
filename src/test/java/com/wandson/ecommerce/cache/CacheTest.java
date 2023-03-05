5fe7deb74a95995fd2ed61386977ee94b5d2eb7c
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2023-03-05 14:54:06 -0300

    Removendo entidades do cache

--- src/test/java/com/wandson/ecommerce/cache/CacheTest.java
+++ src/test/java/com/wandson/ecommerce/cache/CacheTest.java
@@ -1,6 +1,7 @@
 package com.wandson.ecommerce.cache;
 
 import com.wandson.ecommerce.model.Pedido;
+import jakarta.persistence.Cache;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -45,6 +46,27 @@ public class CacheTest {
             entityManager2.find(Pedido.class, 1);
         });
     }
+
+    @Test
+    void removerDoCache() {
+        Assertions.assertDoesNotThrow(() -> {
+            Cache cache = entityManagerFactory.getCache();
+
+            EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+            EntityManager entityManager2 = entityManagerFactory.createEntityManager();
+
+            System.out.println("Buscando a partir da instância 1:");
+            entityManager1.createQuery("select p from Pedido p", Pedido.class).getResultList();
+
+            System.out.println("Removendo do cache");
+            cache.evictAll();
+
+            System.out.println("Buscando a partir da instância 2:");
+            entityManager2.find(Pedido.class, 1);
+            entityManager2.find(Pedido.class, 2);
+        });
+    }
+
 
     @AfterAll
     public static void tearDownAfterClass() {
