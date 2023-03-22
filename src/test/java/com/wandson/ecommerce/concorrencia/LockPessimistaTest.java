2877353cbee02674b77770e4dc6e75ca086e522d
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2023-03-22 18:44:41 -0300

    Fazendo Lock Pessimista com PESSIMISTIC_WRITE

--- src/test/java/com/wandson/ecommerce/concorrencia/LockPessimistaTest.java
+++ src/test/java/com/wandson/ecommerce/concorrencia/LockPessimistaTest.java
@@ -92,6 +92,81 @@ public class LockPessimistaTest {
         log("Encerrando método de teste.");
     }
 
+    @Test
+    void usarLockPessimistaLockModeTypePessimisticWrite() {
+        Runnable runnable1 = () -> {
+            log("Iniciando Runnable 01.");
+
+            var novaDescricao = "Descrição detalhada. CTM: " + System.currentTimeMillis();
+
+            var entityManager = entityManagerFactory.createEntityManager();
+            entityManager.getTransaction().begin();
+
+            log("Runnable 01 vai carregar o produto 1.");
+            var produto = entityManager.find(Produto.class, 1, LockModeType.PESSIMISTIC_WRITE);
+
+            log("Runnable 01 vai alterar o produto.");
+            produto.setDescricao(novaDescricao);
+
+            log("Runnable 01 vai esperar por 3 segundo(s).");
+            esperar(3);
+
+            log("Runnable 01 vai confirmar a transação.");
+            entityManager.getTransaction().commit();
+            entityManager.close();
+
+            log("Encerrando Runnable 01.");
+        };
+
+        Runnable runnable2 = () -> {
+            log("Iniciando Runnable 02.");
+
+            var novaDescricao = "Descrição massa! CTM: " + System.currentTimeMillis();
+
+            var entityManager = entityManagerFactory.createEntityManager();
+            entityManager.getTransaction().begin();
+
+            log("Runnable 02 vai carregar o produto 2.");
+            Produto produto = entityManager.find(Produto.class, 1, LockModeType.PESSIMISTIC_WRITE);
+
+            log("Runnable 02 vai alterar o produto.");
+            produto.setDescricao(novaDescricao);
+
+            log("Runnable 02 vai esperar por 1 segundo(s).");
+            esperar(1);
+
+            log("Runnable 02 vai confirmar a transação.");
+            entityManager.getTransaction().commit();
+            entityManager.close();
+
+            log("Encerrando Runnable 02.");
+        };
+
+        var thread1 = new Thread(runnable1);
+        var thread2 = new Thread(runnable2);
+
+        thread1.start();
+
+        esperar(1);
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        var entityManager = entityManagerFactory.createEntityManager();
+        Produto produto = entityManager.find(Produto.class, 1);
+        entityManager.close();
+
+        Assertions.assertTrue(produto.getDescricao().startsWith("Descrição massa!"));
+
+        log("Encerrando método de teste.");
+    }
+
+
     @AfterAll
     public static void tearDownAfterAll() {
         entityManagerFactory.close();
